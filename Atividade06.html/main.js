a5262eafab5b53126e33c1f77c9084d1f90d2fb0
Paloma <128565943+ypduarte@users.noreply.github.com>
2023-05-16 21:11:42 -0300

    Add files via upload

---
+++ Atividade06.html/main.js
@@ -0,0 +1,21 @@
+//Inicio
+//bootstrap
+src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" 
+
+
+alert("Olá, bem vindo a página!!");
+//variaveis com while
+var contador = 0;
+while(contador < 1) {
+    alert("Oi");
+    contador = contador + 1;
+}
+//input
+var nome;
+nome = prompt("Qual seu nome?")
+alert("Olá, " + nome);
+//função
+function clique_botao() {
+    alert("Você clicou no botão");
+}
